e7d775ab9763b49296de50670c329dbea456555f
Rene Teran <rteran2907@uta.edu.ec>
2023-03-09 00:50:46 -0500

    commit3

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 const Header=()=>{
     return(<div className='gpt3__header section__padding' id="home">
         <div className='gpt3__header-content'>
@@ -9,6 +11,13 @@ const Header=()=>{
                 <input type="email" placeholder='Your Email Address'></input>
                 <button type='button'>Get Started</button>
             </div>
+            <div className='gpt3__header-content__people'>
+                <img src={people} alt='people' /> 
+                <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+                <div className='gtp3__header-image'>
+                    <img src={ai} alt='ai'/>
+                </div>
         </div>
     </div>)
 }
